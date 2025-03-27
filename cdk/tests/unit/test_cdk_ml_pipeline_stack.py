eec47db753d032040c3eedc2d48ef4bc9699529e
Adrian Murillo <adrianmurilloaraya@gmail.com>
2025-03-27 15:38:26 -0600

    Updated ci-cd.yml file for fixing workflow

--- cdk/tests/unit/test_cdk_ml_pipeline_stack.py
+++ cdk/tests/unit/test_cdk_ml_pipeline_stack.py
@@ -1,7 +1,9 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from cdk.cdk.cdk_ml_pipeline import CdkMlPipelineStack
+from cdk.inference_stack import Gpt2InferenceStack
+from cdk.infra_stack import InfraStack
+
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cdk_ml_pipeline/cdk_ml_pipeline_stack.py
