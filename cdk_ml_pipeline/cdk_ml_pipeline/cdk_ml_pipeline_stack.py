9a9a1eaadc2759e62f19f83182036c7bfa9d1905
Adrian Murillo <adrianmurilloaraya@gmail.com>
2025-03-26 19:40:09 -0600

    Scaffold CDK app with dummy S3 + IAM role stack

--- cdk_ml_pipeline/cdk_ml_pipeline/cdk_ml_pipeline_stack.py
+++ cdk_ml_pipeline/cdk_ml_pipeline/cdk_ml_pipeline_stack.py
@@ -1,19 +1,29 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_iam as iam,
 )
 from constructs import Construct
 
-class CdkMlPipelineStack(Stack):
+class CdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # S3 Bucket for model artifacts
+        self.model_artifact_bucket = s3.Bucket(
+            self, "ModelArtifactsBucket",
+            versioned=True,
+            removal_policy=s3.RemovalPolicy.DESTROY,
+            auto_delete_objects=True
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkMlPipelineQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # IAM Role for SageMaker Inference
+        self.sagemaker_execution_role = iam.Role(
+            self, "SageMakerExecutionRole",
+            assumed_by=iam.ServicePrincipal("sagemaker.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSageMakerFullAccess"),
+            ]
+        )
